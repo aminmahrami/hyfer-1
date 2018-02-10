8875fe100a6903706e6f958b50d3db409bceb481
Elias Shikh Alshabab <elias.shkshb1991@gmail.com>
2018-02-10 21:35:48 +0100

    Revert "A lot of work in timeline page + basic structure and router for the r…"

--- src/store/index.js
+++
@@ -1,5 +0,0 @@
-import TimelineStore from './TimeLineStore';
-import ModuleInfoStore from './ModuleInfoStore';
-
-export const timelineStore = (window.timelineStore = new TimelineStore());
-export const moduleInfoStore = (window.moduleInfoStore = new ModuleInfoStore());
